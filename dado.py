2418d2d12d74c09bad3ccf2f4cef7a3411105180
Veronica <veronica.cortes.ceballos@gmail.com>
2016-02-24 19:26:59 +0100

    Comprobando conflictos

--- dado.py
+++ dado.py
@@ -1,10 +1,10 @@
 import math
 class Dado (object):
     def __init__(self, x=0):
-        self.x=x
+        self.x>>x
 
     def __eq__ (self, otro):
-        return self.x== otro.x
+        return self.x>> otro.x
 
     def __str__(self):
         return [self.x,"Caras"]
