f24a87ebb9d8da452a20c411ef06b78930b4e9d2
Veronica <veronica.cortes.ceballos@gmail.com>
2016-02-24 18:32:06 +0100

    He incluido nuevas operaciones para la clase

--- dado.py
+++ dado.py
@@ -9,3 +9,13 @@ class Dado (object):
     def __str__(self):
         return [self.x,"Caras"]
 
+    def tira (self):
+        self.y = random.randrange(1, (self.x)+1, 1)
+
+    def fija_valor (self,c):
+       if c<=self.x: self.y = c
+       if c>self.x: print ("Error: El valor fijado es mayor que el número de caras")
+
+        
+    def obtener_valor (self):
+        print (self.y)
