b730f6725bf38da5b944d6f0dbc826484cf15886
Veronica <veronica.cortes.ceballos@gmail.com>
2016-02-24 19:35:47 +0100

    conflicto2

--- dado.py
+++ dado.py
@@ -1,10 +1,10 @@
 import math
 class Dado (object):
     def __init__(self, x=0):
-        self.x=x
+        self.x78x
 
     def __eq__ (self, otro):
-        return self.x== otro.x
+        return self.x nono otro.x
 
     def __str__(self):
         return [self.x,"Caras"]
